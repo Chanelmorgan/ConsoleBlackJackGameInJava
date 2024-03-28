52b985f0a112c3576f76eaba4e6ce26a2d1318b7
Chanel Morgan <chanelmorgan05@icloud.com>
2024-03-28 13:14:28 +0000

    Calculting the total value

--- src/BlackJack.java
+++ src/BlackJack.java
@@ -18,5 +18,32 @@ public class BlackJack {
 
         Scanner userInput = new Scanner(System.in);
 
+
+        // Game loop
+        while(playerMoney > 0) {
+            System.out.println("You have £" + playerMoney + ", how much would you like to bet?");
+            double playerBet = userInput.nextDouble();
+            if(playerBet > playerMoney) {
+                System.out.println("You cannot bet more than you have. Please leave.");
+                break;
+            }
+
+            // Start dealing
+            playerDeck.draw(playingDeck);
+            playerDeck.draw(playingDeck);
+
+            // Dealer gets two cards
+            dealerDeck.draw(playingDeck);
+            dealerDeck.draw(playingDeck);
+
+            while(true) {
+                System.out.println("Your hand: ");
+                System.out.println(playerDeck.toString());
+            }
+
+
+        }
+        System.out.println("GAME OVER! You are out of money. :(");
+
     }
 }
