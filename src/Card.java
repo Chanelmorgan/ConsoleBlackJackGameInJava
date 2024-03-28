02d6d6f7d9dd6420ea8811a247b82691e6862d33
Chanel Morgan <chanelmorgan05@icloud.com>
2024-03-28 12:39:50 +0000

    Creating the Card Class

--- src/Card.java
+++ src/Card.java
@@ -1,2 +1,21 @@
 public class Card {
+
+    // Variables
+    private Suit suit;
+    private Value value;
+
+    // Constructor
+    public Card(Suit suit, Value value){
+        this.value = value;
+        this.suit = suit;
+    }
+
+
+    public String toString(){
+        return this.suit.toString() + "-" + this.value.toString();
+    }
+
+    public Value getValue(){
+        return this.value;
+    }
 }
