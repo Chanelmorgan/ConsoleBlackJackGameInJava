31df1d1e827728bc182f214c933d109cc9659fcf
Chanel Morgan <chanelmorgan05@icloud.com>
2024-03-28 13:02:23 +0000

    Getting the user input from the console

--- src/BlackJack.java
+++ src/BlackJack.java
@@ -1,13 +1,22 @@
+import java.util.Scanner;
 
 public class BlackJack {
 
     public static void main(String[] args) {
         System.out.println("Welcome to BlackJack!");
 
-        // Create our playing deck
+        // Create playing deck
         Deck playingDeck = new Deck();
         playingDeck.generateDeck();
         playingDeck.shuffle();
-        System.out.println(playingDeck);
+
+        // Create a deck for the player -> the cards the player has in hand
+        Deck playerDeck = new Deck();
+        Deck dealerDeck = new Deck();
+
+        double playerMoney = 100.00;
+
+        Scanner userInput = new Scanner(System.in);
+
     }
 }
