46b2ea3c0873e5aa4f2429e7dc8a8591c5489149
Chanel Morgan <chanelmorgan05@icloud.com>
2024-03-28 12:54:02 +0000

    Creating the deck class and the main class

--- src/BlackJack.java
+++ src/BlackJack.java
@@ -1,2 +1,12 @@
+
 public class BlackJack {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to BlackJack!");
+
+        // Create our playing deck
+        Deck playingDeck = new Deck();
+        playingDeck.generateDeck();
+        System.out.println(playingDeck);
+    }
 }
--- src/Deck.java
+++ src/Deck.java
@@ -1,2 +1,53 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Deck {
+
+    // Instance vars
+    private ArrayList<Card> cards;
+
+    // Constructor
+    public Deck() {
+        this.cards = new ArrayList<Card>();
+    }
+
+    public void generateDeck() {
+        //Generate Cards
+        for (Suit cardSuit : Suit.values()) {
+            for (Value cardValue : Value.values()) {
+                // Add a new card to the deck
+                this.cards.add(new Card(cardSuit, cardValue));
+            }
+        }
+    }
+
+    public void shuffle(){
+        ArrayList<Card> tempDeck = new ArrayList<Card>();
+        // Use Random -> to generate random numners
+        Random random = new Random();
+        int randomCardIndex = 0;
+        int originalSize = this.cards.size();
+
+        for(int i =0; i < originalSize; i++) {
+            // Generate random index
+            randomCardIndex =  random.nextInt((this.cards.size() - 1)+1);
+            tempDeck.add(this.cards.get(randomCardIndex));
+            // Remove from original deck
+            this.cards.remove(randomCardIndex);
+        }
+
+        this.cards = tempDeck;
+    }
+
+    public String toString() {
+        String cardListOutput = "";
+        int i = 0;
+        for (Card card : this.cards) {
+            cardListOutput += "\n" + i + "-" + card.toString();
+            i++;
+        }
+        return cardListOutput;
+    }
+
+
 }
